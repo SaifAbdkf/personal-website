6744499d88f23a99df71618193550530bf9f2280
Saif <seif.abdelkefi@gmail.com>
2025-02-02 11:50:08 +0100

    added a header section with a profile picure

---
+++ saif-website/src/Header.tsx
@@ -0,0 +1,21 @@
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className={"headerContainer"}>
+      <img src="../assets/headshot3.jpg" className={"headshot"} />
+      <div className={"title"}>
+        <div> Saif Abdelkefi</div>
+        <div> Sofware Engineer</div>
+      </div>
+      <div className={"links"}>
+        <div> CV </div>
+        {/* download cv with cool animation*/}
+        <div> github </div>
+        {/* checkout my github */}
+        <div> linkedin </div>
+        {/* lets connect on linkedin */}
+      </div>
+    </div>
+  );
+}
--- saif-website/src/main.tsx
+++ saif-website/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import "./index.css";
+import Header from "./Header";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className={"mainContainer"}>
+      <div className={"content"}>
+        <Header />
+      </div>
+    </div>
   </StrictMode>
 );
