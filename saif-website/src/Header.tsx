fb19e37cd9cb6e53575b79b56bb3fe5b5ebd78b6
Mohamed-Rokn <mohamedabdelkefi66@gmail.com>
2025-02-02 16:56:33 +0100

    Added NAVBAR

--- saif-website/src/Header.tsx
+++ saif-website/src/Header.tsx
@@ -2,19 +2,33 @@ import "./Header.css";
 
 export default function Header() {
   return (
-    <div className={"headerContainer"}>
-      <img src="../assets/headshot3.jpg" className={"headshot"} />
-      <div className={"title"}>
-        <div className="saif_H1"> Saif Abdelkefi</div>
-        <div> Sofware Engineer</div>
+    <div className="headerContainer">
+      <img src="../assets/headshot3.jpg" className="headshot" alt="Saif Abdelkefi" />
+      <div className="title">
+        <div className="saif_H1">Saif Abdelkefi</div>
+        <div>Software Engineer</div>
       </div>
-      <div className={"links"}>
-        <div> CV </div>
-        {/* download cv with cool animation*/}
-        <div> github </div>
-        {/* checkout my github */}
-        <div> linkedin </div>
-        {/* lets connect on linkedin */}
+      <div className="links">
+        <div>CV</div>
+        <div>GitHub</div>
+        <div>LinkedIn</div>
+      </div>
+      <div className="menu-container">
+        <a href="/" className="menu-item">
+          <button className="menu-button">
+            <span className="button-text">WhoAmI</span>
+          </button>
+        </a>
+        <a href="/blogs" className="menu-item">
+          <button className="menu-button">
+            <span className="button-text">Experience</span>
+          </button>
+        </a>
+        <a href="/newsletter" className="menu-item">
+          <button className="menu-button">
+            <span className="button-text">Projects</span>
+          </button>
+        </a>
       </div>
     </div>
   );
