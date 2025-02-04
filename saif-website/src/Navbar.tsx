e83dbb18387115004257551a26f08e4908eb2a97
Saif <seif.abdelkefi@gmail.com>
2025-02-04 11:17:09 +0100

    color change rectangular

--- saif-website/src/Navbar.tsx
+++ saif-website/src/Navbar.tsx
@@ -2,26 +2,38 @@ import { Link, useLocation } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-  const location = useLocation(); // Récupérer le chemin actuel
+  const location = useLocation();
 
   return (
     <div className="navigationContainer">
       <hr />
       <div className="menu-container">
         <Link to="/">
-          <button className={`menu-button ${location.pathname === "/" ? "active" : ""}`}>
+          <button
+            className={`menu-button ${
+              location.pathname === "/" ? "active" : ""
+            }`}
+          >
             WhoAmI
           </button>
         </Link>
 
         <Link to="/experience">
-          <button className={`menu-button ${location.pathname === "/experience" ? "active" : ""}`}>
+          <button
+            className={`menu-button ${
+              location.pathname === "/experience" ? "active" : ""
+            }`}
+          >
             Experience
           </button>
         </Link>
 
         <Link to="/projects">
-          <button className={`menu-button ${location.pathname === "/projects" ? "active" : ""}`}>
+          <button
+            className={`menu-button ${
+              location.pathname === "/projects" ? "active" : ""
+            }`}
+          >
             Projects
           </button>
         </Link>
